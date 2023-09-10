1a2c32cf62623488e676dd0d68d5aeaade73bc65
nisimben <59839685+nisimben@users.noreply.github.com>
2023-09-10 16:28:22 +0300

    Update test.ts

--- upload-webapp/src/test.ts
+++ upload-webapp/src/test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
@@ -13,19 +12,3 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-=======
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
->>>>>>> 2cfd18384cffe46cb45fed2b848bf6c2824d438d
